71b7af6d318c1bc6c23ef06f10dc4e866b7bde57
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2023-07-28 17:17:59 +0530

    Update sd_api.py

--- sd_api.py
+++ sd_api.py
@@ -9,14 +9,14 @@ async def inpaint(image: bytes, mask: bytes) -> bytes:
     async with httpx.AsyncClient(timeout=None) as client:
         r = await client.post(
             #"http://localhost:7861/sdapi/v1/img2img",
-            "https://106f-34-83-53-225.ngrok.io/sdapi/v1/img2img",
+            "https://849c-34-90-88-222.ngrok.io/sdapi/v1/img2img",
             json={
                 "init_images": [image],
                 "resize_mode": "1",
                 "denoising_strength": 0.9,
                 "mask": mask,
                 "mask_blur": 8,
-                "prompt": "(make her nude, completely nude), perfect sexy breast, midium breasts, sexy body, soft tighs, intricate skin texture, (goosebumps:1.3)",
+                "prompt": "(make her nude, completely nude), nude girl, perfect sexy breast, midium breasts, sexy body, shaved pussy, soft tighs, intricate skin texture, (goosebumps:1.3)",
                 #"prompt": "make her nude below face, completely nude, sexy girl, perfect sexy breast, perfect hands, realistic hands, extremely detailed hands, individual fingers, intricate fingers, 8k hands, perfect body, realistic body, extremely detailed body, 8k body hyper detailed, intricate skin texture, unreal engine, (goosebumps:1.3)",
                 "negative_prompt": "(deformed, deformed body, deformed breasts, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, (low quality, worst quality), EasyNegativeV2",
                 "steps": 24,
