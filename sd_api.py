10b3a9805ae84f5d8f42732530871bf7a1a62023
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2023-07-30 09:04:58 +0530

    Update sd_api.py

--- sd_api.py
+++ sd_api.py
@@ -9,20 +9,21 @@ async def inpaint(image: bytes, mask: bytes) -> bytes:
     async with httpx.AsyncClient(timeout=None) as client:
         r = await client.post(
             #"http://localhost:7861/sdapi/v1/img2img",
-            "https://19d9-34-145-91-128.ngrok.io/sdapi/v1/img2img",
+            "https://bde1-34-168-222-190.ngrok.io/sdapi/v1/img2img",
             json={
                 "init_images": [image],
                 "resize_mode": "1",
                 "denoising_strength": 0.9,
                 "mask": mask,
                 "mask_blur": 8,
-                "prompt": "nude girl, undress her, 44 G cup chest, erect nipples, sexy body, shaved pussy, detailed vagina, soft tighs, intricate skin texture, smooth skin, natural skin tone, realistic, nsfw, (goosebumps:1.3)",
+                "prompt": "(nude girl, undress her, sexy body, sexy breasts), erect nipples, shaved pussy, detailed vagina, soft tighs, intricate skin texture, smooth skin, natural skin tone, realistic, nsfw, (goosebumps:1.3)",
                 #"prompt": "naked girl, bare naked, (make her nude, completely nude), nude girl, perfect sexy breast, midium breasts, highly detailed breasts, sexy body, shaved pussy, detailed vagina, soft tighs, intricate skin texture, (goosebumps:1.3)",
                 #"prompt": "make her nude below face, completely nude, sexy girl, perfect sexy breast, perfect hands, realistic hands, extremely detailed hands, individual fingers, intricate fingers, 8k hands, perfect body, realistic body, extremely detailed body, 8k body hyper detailed, intricate skin texture, unreal engine, (goosebumps:1.3)",
-                "negative_prompt": "inpainting trace, border lines, (deformed, deformed body, deformed breasts, distorted, disfigured:1.3), black border, poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, (low quality, worst quality)",
+                "negative_prompt": "inpainting trace, border lines, (deformed, deformed body, distorted, disfigured:1.3), black border, poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, badhandv4, (low quality, worst quality)",
                 "steps": 24,
+                "sampler_index": "DPM++ 2M Karras",
                 #"sampler_index": "DPM++ 2M SDE Karras",
-                "sampler_index": "Euler a",
+                #"sampler_index": "Euler a",
                 "inpaint_full_res": False,
             }
         )
