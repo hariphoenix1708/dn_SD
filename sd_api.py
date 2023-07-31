6676f212256d7aae733380ffb0ad115d4de42f38
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2023-07-31 10:08:23 +0530

    Update sd_api.py

--- sd_api.py
+++ sd_api.py
@@ -8,8 +8,8 @@ async def inpaint(image: bytes, mask: bytes) -> bytes:
     async with httpx.AsyncClient(timeout=None) as client:
         r = await client.post(
             #"http://localhost:7861/sdapi/v1/img2img",
-            #"https://ae93-34-124-139-173.ngrok.io/sdapi/v1/img2img",
-            "https://comic-caribou-frankly.ngrok-free.app/sdapi/v1/img2img",
+            " https://873d-61-0-126-165.ngrok-free.app/sdapi/v1/img2img",
+            #"https://comic-caribou-frankly.ngrok-free.app/sdapi/v1/img2img",
             json={
                 "init_images": [image],
                 "resize_mode": "1",
